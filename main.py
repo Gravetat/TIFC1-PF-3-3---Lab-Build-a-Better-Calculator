172b542e9a1495fc791f74f868e12288566d5762
Gravetat <danielaobando1@outlook.com>
2025-11-14 14:44:06 -0500

    Agrego funciones de calculadora y pruebas corregidas

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
 
-if __name__=="__main__":
-  main()
+
+def multiplymultiplenumbers(numbers):
+    result = 1
+    for num in numbers:
+        result *= num
+    return result
+
+
+def isiteven(num):
+    if isinstance(num, int) and num % 2 == 0:
+        return True
+    return False
+
+
+def isitaninteger(num):
+    return isinstance(num, int)
+
+
+def main():
+    print("Hello learners!")
+    
+    
+    print(addmultiplenumbers([1, 2, 3]))            
+    print(multiplymultiplenumbers([2, 3, 4]))       
+    print(isiteven(5))                               
+    print(isitaninteger(10))                         
+
+
+if __name__ == "__main__":
+    main()
